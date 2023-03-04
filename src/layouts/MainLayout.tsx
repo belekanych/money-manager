c535486510dcbc6402370501a69abc99545be4f7
belekanych <yuriy.belekanych@gmail.com>
2023-03-05 00:29:41 +0200

    Fixed navigation styles.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,34 +1,30 @@
 import { Outlet, Link } from "react-router-dom"
-import { TfiSettings, TfiPlus, TfiViewList } from 'react-icons/tfi';
+import { TfiSettings, TfiPlus, TfiViewList } from "react-icons/tfi"
 
 function MainLayout() {
   const links = [
     {
-      href: '/transactions',
+      href: "/transactions",
       icon: <TfiViewList />,
     },
     {
-      href: '/transactions/create',
+      href: "/transactions/create",
       icon: <TfiPlus />,
     },
     {
-      href: '/settings',
+      href: "/settings",
       icon: <TfiSettings />,
     },
-  ];
+  ]
 
   return (
-    <div className="bg-gray-50 h-screen w-full flex flex-col justify-between">
-      <main className="overflow-scroll">
+    <div className="h-screen w-full">
+      <main className="overflow-scroll pb-12">
         <Outlet />
       </main>
-      <nav className="bg-white shadow flex flex-row justify-around">
-        {links.map(link => (
-          <Link
-            to={link.href}
-            key={link.href}
-            className="p-4 text-black"
-          >
+      <nav className="bg-white shadow fixed bottom-0 w-full flex flex-row justify-around">
+        {links.map((link) => (
+          <Link to={link.href} key={link.href} className="p-4 text-black">
             {link.icon}
           </Link>
         ))}
